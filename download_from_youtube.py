fafe70de25e7acca67f8e69be74e92c15db7b1ea
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-07-06 15:07:30 +0300

    window size update

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -13,17 +13,20 @@ entry.grid(row=0, column=1,  padx=10, pady=20)
 update_label = Label(root, foreground='black', background='#856ff8', font=BOLD)
 update_label.grid(row=1,column=1)  
 
+
 def main():
-    root.geometry("500x200") 
+    root.geometry("580x200") 
     root.resizable(False, False)
     root.title('Download from youtube')
     root.configure(background='#856ff8')
 
     label = Label(root, text="Enter youtube link :", foreground='black', background='#856ff8', font=BOLD)
     label.grid(row=0,column=0,  padx=10, pady=20)
+    
    
     button = Button(root, text="Download", background='#856ff8', foreground='black', font=BOLD, command=lambda:thread(entry.get(), 'C:/Users/{}/Downloads'.format(getpass.getuser())))
     button.grid(row=0, column=3, padx=10, pady=20)
+    
 
     root.mainloop()
 
@@ -43,6 +46,7 @@ def downloadYouTube(videourl, path):
         update_label.configure(text='download successfully!')
     except:
         update_label.configure(text='download failed!')
+
 
 if __name__ == '__main__':
     main()
