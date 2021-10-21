57624cc18af8a62f5a493fd396dc25deed81c933
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-10-21 11:28:57 +0300

    pytube res -> https() 410 e

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -1,74 +1,55 @@
 from pytube import YouTube
 from tkinter.font import BOLD
 from tkinter import *
-import os
 import threading
 import getpass
+import os
 
-selection = ['Highest quality', 'Regular quality', 'Lowest quality']
 row0 = 0; row1 = 1; row2 = 2
+color = '#FFAE42'
 
 root = Tk()
 
-optionVar = StringVar()
-optionVar.set(selection[1])
-
-update_label = Label(root, foreground='black', background='#856ff8', font=BOLD)
-update_label.grid(row=1,column=1)  
+update_label = Label(root, foreground='black', background=color, font=BOLD)
+update_label.grid(row=row2,column=1)  
 
 def main():
     root.geometry("580x200") 
     root.resizable(False, False)
-    root.title('Download from youtube')
-    root.configure(background='#856ff8')
+    root.title('Download from YouTube')
+    root.configure(background=color)
     
-    highlight = Label(root, text="Download from Youtube!", foreground='black', background='#856ff8', font=BOLD)
+    highlight = Label(root, text="Download from Youtube!", foreground='black', background=color, font=BOLD)
     highlight.grid(row=row0,column=1,  padx=10, pady=20)
 
-    options = OptionMenu(root, optionVar, *selection)
-    options.config(font=BOLD, background='#856ff8', width=12)
-    options.grid(row=row1,column=0)
-
-    label = Label(root, text="Enter youtube link :", foreground='black', background='#856ff8', font=BOLD)
-    label.grid(row=row2,column=0,  padx=10, pady=20)
+    label = Label(root, text="Enter youtube link :", foreground='black', background=color, font=BOLD)
+    label.grid(row=row1,column=0,  padx=10, pady=20)
 
     entry = Entry(root, font=BOLD)
-    entry.grid(row=row2, column=1, padx=10, pady=20)
+    entry.grid(row=row1, column=1, padx=10, pady=20)
 
-    button = Button(root, text="Download", background='#856ff8', foreground='black', font=BOLD, command=lambda:thread(entry.get(), 'C:/Users/{}/Downloads'.format(getpass.getuser())))
-    button.grid(row=row2, column=3, padx=10, pady=20)
+    button = Button(root, text="Download", background=color, foreground='black', font=BOLD, command=lambda:thread(entry.get(), 'C:/Users/{}/Downloads'.format(getpass.getuser())))
+    button.grid(row=row1, column=2, padx=10, pady=20)
     
     root.mainloop()
 
 def thread(videourl, path):
-
-    print(optionVar.get())
-
-    update_label.configure(text='downloading..')
-    t1 = threading.Thread(target=downloadYouTube, args=(videourl, path))
+    update_label.configure(text='downloading to\n' + path)
+    t1 = threading.Thread(target=downloadYouTube, args=(videourl, path,))
     t1.start()
 
 def downloadYouTube(videourl, path):
     try:
-        yt = YouTube(videourl)
-        yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-        
-        yt = selectResolution(yt)
+        youTube = YouTube(videourl)
        
         if not os.path.exists(path):
             os.makedirs(path)
-        yt.download(path)
+
+        youTube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().get_highest_resolution().download(path)
         update_label.configure(text='download successfully!')
-    except:
+    except Exception as ex:
+        print(type(ex).__name__, ex.args)
         update_label.configure(text='download failed!')
-
-def selectResolution(yt):
-    if(optionVar.get() == selection[0]):
-        return yt.get_highest_resolution()
-    elif(optionVar.get() == selection[1]):
-        return yt.get_by_resolution()
-    else:
-       return yt.get_lowest_resolution()       
-
+   
 if __name__ == '__main__':
     main()
